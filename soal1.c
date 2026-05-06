4d0be7b875c04d8e0c0110912b16cf77088145d3
nagelss <hufnagelrls@gmail.com>
2026-05-06 08:26:53 +0700

    commit1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,54 @@
 #include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 4
+ *   Hari dan Tanggal    : Rabu, 6 Mei 2026
+ *   Nama (NIM)          : Hufnagel Ruben Lenard Situmorang (13224015)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Menghitung total waktu antrian pasien
+ * 
+ */
 
-int main(void) {
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int queue[100];
+
+typedef struct Pasien{
+    char id[10];
+    int waktu;
+} Pasien;
+
+int rekursi(int n){
+    int sum = 0;
+    for (int i=0;i<n;i++){
+        sum += queue[i];
+    }
+    return sum;
+}
+
+int main(){
+    int N;
+    scanf("%d",&N);
+
+    Pasien listpasien[N];
+    for (int i=0;i<N;i++){
+        scanf("%s",&listpasien[i].id);
+        scanf("%d",&listpasien[i].waktu);
+        queue[i] = listpasien[i].waktu;
+    }
+
+    printf("ORDER ");
+    for (int i=0;i<N;i++){
+        printf("%s ",listpasien[i].id);
+    }
+    
+    int sum = 0;
+    for (int i=0;i<N;i++){
+        sum += rekursi(i);
+    }
+    printf("\nWAKTU %d",sum);
     return 0;
+
 }
